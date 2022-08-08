8a5301e74f49d83f93e4524b0b6db9355c6b1adb
Grisha <dinozzzzzawrik@gmail.com>
2022-08-08 19:58:10 +0300

    add class DB and logic

--- database.py
+++ database.py
@@ -1,18 +1,33 @@
 import psycopg2
 from data import config
 
-conn = psycopg2.connect(host=config.host, port=config.port, database=config.database, user=config.user, password=config.password)
-cur = conn.cursor()
-print("Database opened successfully")
-
 
 class BD:
-    def __init__(self):
-        self.id = None
-        self.nickname_tg = None
+    try:
+        connection = psycopg2.connect(host=config.host, port=config.port, database=config.database, user=config.user,
+                                      password=config.password)
+        connection.autocommit = True
 
-    def add_id(self):
-        cur.execute("""SELECT * FROM users""")
-        query_results = cur.fetchall()
-        text = '\n\n'.join([', '.join(map(str, x)) for x in query_results])
-        return str(text)
+        with connection.cursor() as cursor:
+            cursor.execute(
+                 """CREATE TABLE IF NOT EXISTS users(
+                     id serial PRIMARY KEY,
+                     tg_id varchar(50) NOT NULL,
+                     dota_id varchar(50) NOT NULL);"""
+            )
+            print("[INFO] Table created successfully")
+
+        @staticmethod
+        def add_id(dota_id, tg_id):
+            connection = psycopg2.connect(host=config.host, port=config.port, database=config.database,
+                                          user=config.user,
+                                          password=config.password)
+            connection.autocommit = True
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    f"""INSERT INTO users (tg_id, dota_id) VALUES
+                    ({tg_id}, {dota_id});"""
+                )
+
+    except (Exception, psycopg2.Error) as error:
+        print(f"[ERROR] Error while connecting to PostgreSQL: {error}")
