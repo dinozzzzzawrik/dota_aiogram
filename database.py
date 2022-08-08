24dffdded83847cb088929e0863ebd21a5f4765c
Grisha <dinozzzzzawrik@gmail.com>
2022-08-08 16:10:59 +0300

    add vars from config

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import psycopg2
+from data import config
 
-conn = psycopg2.connect(host="localhost", port=5432, database="bot", user="postgres", password="postgres")
+conn = psycopg2.connect(host=config.host, port=config.port, database=config.database, user=config.user, password=config.password)
 cur = conn.cursor()
 print("Database opened successfully")
 
@@ -14,4 +15,4 @@ class BD:
         cur.execute("""SELECT * FROM users""")
         query_results = cur.fetchall()
         text = '\n\n'.join([', '.join(map(str, x)) for x in query_results])
-        return (str(text))
+        return str(text)
