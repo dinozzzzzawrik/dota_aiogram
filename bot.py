8b3ffc93482c3085eea4afe822731bd33acbb66b
Grisha <dinozzzzzawrik@gmail.com>
2022-08-08 20:15:24 +0300

    add func check_id

--- bot.py
+++ bot.py
@@ -31,29 +31,46 @@ async def add_id(message: types.Message):
         await message.reply(e)
 
 
-@dp.message_handler()
-async def check(message: types.Message):
-    msg = message.text
+def main(dota_id):
     main_info = ''
     status = True
     try:
-        response = requests.get(config.LINK + msg)
+        response = requests.get(config.LINK + dota_id)
         data = response.json()
-        main_info = 'Ник: ' + str(Profile.from_json(data).personaname) + '\n' + 'Айди: ' + str(Profile.from_json(data).account_id) + '\n' + 'Статус Dota+: ' + str(Profile.from_json(data).plus) + '\n' + 'Страна: ' + str(Profile.from_json(data).country) + '\n'
+        main_info = 'Ник: ' + str(Profile.from_json(data).personaname) + '\n' + 'Айди: ' + str(
+            Profile.from_json(data).account_id) + '\n' + 'Статус Dota+: ' + str(
+            Profile.from_json(data).plus) + '\n' + 'Страна: ' + str(Profile.from_json(data).country) + '\n'
     except KeyError:
         status = False
 
     try:
-        response = requests.get(config.LINK + msg + '/wl')
+        response = requests.get(config.LINK + dota_id + '/wl')
         data = response.json()
-        winrate = 'Победы: ' + str(Winrate.from_json(data).win) + '\n' + 'Поражения: ' + str(Winrate.from_json(data).lose)
+        winrate = 'Победы: ' + str(Winrate.from_json(data).win) + '\n' + 'Поражения: ' + str(
+            Winrate.from_json(data).lose)
     except KeyError:
         winrate = 'Победы: 0\n' + 'Поражения: 0'
 
     if status:
-        await message.reply(main_info + '\n' + winrate)
+        return main_info + '\n' + winrate
     else:
-        await message.reply("Нет такого аккаунта или у него закрытый доступ к статистике матчей.")
+        return "Нет такого аккаунта или у него закрытый доступ к статистике матчей."
+
+
+@dp.message_handler(commands=['check_id'])
+async def check_id(message: types.Message):
+    tg_id = message.from_user.id
+    try:
+        result = BD.check_id(tg_id)
+        await message.reply(main(result))
+    except Exception as e:
+        await message.reply("Ошибка при проверке ID в базе данных.")
+
+
+@dp.message_handler()
+async def check(message: types.Message):
+    msg = message.text
+    await message.reply(main(msg))
 
 
 if __name__ == '__main__':
